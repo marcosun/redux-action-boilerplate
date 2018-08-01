abec93aafca845c03d6096cd2d279e1404aa4a51
marcosun <marcoinbristol@gmail.com>
2018-08-01 14:42:30 +0800

    [core] Export build outputs with three module systems

--- script/copy-files.js
+++ script/copy-files.js
@@ -1,16 +1,43 @@
 /* eslint-disable */
-
 import path from 'path';
 import fse from 'fs-extra';
 
 async function copyFile(file) {
   const buildPath = path.resolve(__dirname, '../build/', path.basename(file));
-  await fse.copy(file, buildPath);
-  console.log(`Copied ${file} to ${buildPath}`);
+
+  console.log(`Copying ${file} to ${buildPath}`);
+
+  try {
+    await fse.copy(file, buildPath);
+    console.log(`${file} OK!`);
+  } catch(e) {
+    console.error(`${file} FAILED!!!`);
+  }
+}
+
+async function createPackageFile() {
+  const packageData = await fse.readFile(path.resolve(__dirname, '../package.json'), 'utf8');
+  const {scripts, devDependencies, ...packageDataOther } = JSON.parse(
+    packageData,
+  );
+
+  const newPackageData = {
+    ...packageDataOther,
+    main: './index.js',
+    module: './index.es.js',
+  };
+
+  const buildPath = path.resolve(__dirname, '../build/package.json');
+
+  await fse.writeFile(buildPath, JSON.stringify(newPackageData, null, 2), 'utf8');
+
+  return newPackageData;
 }
 
 async function run() {
-  await ['README.md', 'LICENSE', 'package.json'].map(file => copyFile(file));
+  await ['README.md', 'LICENSE'].map(file => copyFile(file));
+
+  await createPackageFile();
 }
 
 run();
